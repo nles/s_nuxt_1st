1d2590888c10dbfac5984f3640b13aa1f1c05b43
Nikke Leskelä <nileskel@gmail.com>
2018-12-02 16:02:45 +0000

    Update eject.sh script.

--- templates/scripts/eject.sh
+++ templates/scripts/eject.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $SCRIPT_DIR
+cd ..
 echo "Fetching stuff from inside the container."
 
 IMAGE_NAME=s_nuxt_1st
@@ -7,9 +10,17 @@ if [ ! -f $RUNNER ]; then exit 1; fi
 CONTAINER_NAME=${IMAGE_NAME}_tmp
 CONTAINER_NAME=$CONTAINER_NAME NO_TTY=1 $RUNNER eval "sleep 30;" &
 
-sleep 3
+echo "run container..."
+until docker ps | grep "$CONTAINER_NAME" > /dev/null; do
+  sleep 2
+  echo "waiting for container to start..."
+done
+echo "container started!"
 
 docker cp $CONTAINER_NAME:/$IMAGE_NAME/node_modules ./
 docker cp $CONTAINER_NAME:/$IMAGE_NAME/.eslintrc.js ./
 docker cp $CONTAINER_NAME:/$IMAGE_NAME/.prettierrc ./
 docker cp $CONTAINER_NAME:/$IMAGE_NAME/tsconfig.json ./
+
+echo "files copied."
+docker stop $CONTAINER_NAME
