188233e1f098ffaa53c8c3a0a56625e6ae9a9854
Nikke Leskelä <nileskel@gmail.com>
2018-12-23 16:37:13 +0000

    Move example text.

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -6,6 +6,12 @@ fi
 
 # remove previous build if exists
 rm -rf $PROJECT_ROOT_PATH/node_modules*
-rm $PROJECT_ROOT_PATH/.ejected
+rm -f $PROJECT_ROOT_PATH/.ejected
 
 $PROJECT_ROOT_PATH/scripts/build.sh
+
+echo
+echo "To start with example base, run:"
+echo "cp ./dependencies/s_nuxt_1st/example/src ./src"
+echo "./s_nuxt_1st yarn add axios"
+echo
